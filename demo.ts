395917cf3d187083f7d5524330de27778a9ecab7
MohammedZuhairAhmed <mohammed.ahmed@contentstack.com>
2024-05-27 10:54:07 +0530

    MIGRATION : JS-SDK to TS-SDK

--- demo.ts
+++ demo.ts
@@ -3,8 +3,8 @@ import { generateTS } from "./src/generateTS/index";
 async function fetch() {
   try {
     const res = await generateTS({
-      token: "csbb041a11ccf2182c87d74b2d",
-      apiKey: "bltb07d61d76cca54b3",
+      token: "csbb041a11ccf2182c87d74b2",
+      apiKey: "bltb07d61d76cca543",
       environment: "development",
       region: "US",
       tokenType: "delivery",
